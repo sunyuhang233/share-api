1c4ae4c37bcd75499e82e4d81a2dac9f5ac4aa09
hang <2283582431@qq.com>
2023-10-07 08:33:16 +0800

    修改启动日志 增加/user-service上下文 配置logback日志

--- share-user/src/main/java/top/hang/share/user/config/UserApplication.java
+++ share-user/src/main/java/top/hang/share/user/config/UserApplication.java
@@ -1,8 +1,10 @@
 package top.hang.share.user.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author : Ahang
@@ -12,8 +14,12 @@ import org.springframework.context.annotation.ComponentScan;
  **/
 @SpringBootApplication
 @ComponentScan("top.hang")
+@Slf4j
 public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class,args);
+        SpringApplication app = new SpringApplication(UserApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("测试地址:http://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 }
