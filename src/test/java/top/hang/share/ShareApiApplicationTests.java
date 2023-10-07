ec21f99dc642e913d161ea8f774f80f089c4f8ea
hang <2283582431@qq.com>
2023-10-07 08:14:14 +0800

    初始化项目

---
+++ src/test/java/top/hang/share/ShareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.hang.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
