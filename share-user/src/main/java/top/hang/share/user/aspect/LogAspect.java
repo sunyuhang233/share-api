713f6cca329aacf2233dd795c6c0d0a19db013e5
hang <2283582431@qq.com>
2023-10-07 10:23:08 +0800

    增加 LogAspect，打印请求参数和返回结果及日志流水号

---
+++ share-user/src/main/java/top/hang/share/user/aspect/LogAspect.java
@@ -0,0 +1,96 @@
+package top.hang.share.user.aspect;
+
+import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author : Ahang
+ * @program : share-api
+ * @description : 自定义切点
+ * @create : 2023-10-07 08:39
+ **/
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+    public LogAspect() {
+        log.info("LogAspect");
+    }
+
+    /***
+     * @description 定义一个切点
+     *
+     * @return null
+     */
+    @Pointcut("execution(public * top.hang..*Controller.*(..))")
+    public void controllerPointcut() {
+    }
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) {
+        // 增加日志的流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+
+        // 开始打印日志请求
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert attributes != null;
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        log.info("-----------------开始------------------");
+        log.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        log.info("类名方法: {} {}", signature.getDeclaringTypeName(), name);
+        log.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+
+        // 排除特殊类型的参数 如文件类型
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest || args[i] instanceof ServletResponse || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段 敏感字段或太长不显示:身份证 手机号 邮箱 密码等
+        String[] excludeProperties = {"phone", "password"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filters.addFilter();
+        excludeFilter.addExcludes(excludeProperties);
+        log.info("请求参数: {}", JSONObject.toJSONString(arguments, excludeFilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段 敏感字段或太长不显示:身份证 手机号 邮箱 密码等
+        String[] excludeProperties = {"phone", "password"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filters.addFilter();
+        excludeFilter.addExcludes(excludeProperties);
+        log.info("返回结果: {}", JSONObject.toJSONString(result, excludeFilter));
+        log.info("----------------结束 耗时: {}ms---------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+}
